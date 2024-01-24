f46d3cefb2993921be0a15198c3087354b572907
kerry918 <kerryliukerui@gmail.com>
2024-01-24 15:50:09 -0500

    fix

--- src/page/Onboarding/StepOne.tsx
+++ src/page/Onboarding/StepOne.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
