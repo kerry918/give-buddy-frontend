d571141e5a0e02b362325f9f9850b7dadfdcee60
kerry918 <kerryliukerui@gmail.com>
2024-01-24 13:34:24 -0500

    remove unused import

--- src/page/Onboarding/StepOne.tsx
+++ src/page/Onboarding/StepOne.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
