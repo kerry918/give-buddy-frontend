e5124061790936d016f132eb6e434f2d65052dd8
kerry918 <kerryliukerui@gmail.com>
2024-01-24 16:12:50 -0500

    remove city and update province dropdown

---
+++ src/constants/province.js
@@ -0,0 +1,60 @@
+const availableProvince = [
+  {
+    long: "AB - Alberta",
+    short: "AB"
+  }, 
+  {
+    long: "BC - British Columbia",
+    short: "BC"
+  }, 
+  {
+    long: "MB - Manitoba",
+    short: "MB"
+  }, 
+  {
+    long: "BC - British Columbia",
+    short: "BC"
+  }, 
+  {
+    long: "NB - New Brunswick",
+    short: "NB"
+  }, 
+  {
+    long: "NL - Newfoundland and Labrador",
+    short: "NL"
+  }, 
+  {
+    long: "NS - Nova Scotia",
+    short: "NS"
+  }, 
+  {
+    long: "NT - Northwest Territories",
+    short: "NT"
+  }, 
+  {
+    long: "NU - Nunavut",
+    short: "NU"
+  }, 
+  {
+    long: "ON - Ontario",
+    short: "ON"
+  }, 
+  {
+    long: "PE - Prince Edward Island",
+    short: "PE"
+  }, 
+  {
+    long: "QC - Quebec",
+    short: "QC"
+  }, 
+  {
+    long: "SK - Saskatchewan",
+    short: "SK"
+  }, 
+  {
+    long: "YT - Yukon",
+    short: "YT"
+  }, 
+]
+
+export default availableProvince
