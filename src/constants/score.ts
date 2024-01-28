720f074cadd44697c9a20b437ed0e39f51006717
kerry918 <kerryliukerui@gmail.com>
2024-01-28 13:44:03 -0500

    finalize the recommendation page

---
+++ src/constants/score.ts
@@ -0,0 +1,14 @@
+export const financialTransparency:Record<string, string> = {
+  100: "Level 2", 
+  50: "Level 1", 
+  0: "Level 0"
+}
+
+export const resultsReporting:Record<string, string> = {
+  0: "B-", 
+  20: "B",
+  40: "B+",
+  60: "A-",
+  80: "A",
+  100: "A+"
+}
