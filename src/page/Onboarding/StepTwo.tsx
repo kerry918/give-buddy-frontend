ed258e1bb29cfa43cf5936b5bca440b77bae70d8
kerry918 <kerryliukerui@gmail.com>
2024-03-17 21:23:05 -0400

    add icon to category

--- src/page/Onboarding/StepTwo.tsx
+++ src/page/Onboarding/StepTwo.tsx
@@ -4,6 +4,14 @@ import Button from '@mui/material/Button';
 import { useGiveBuddyStore } from '../../store/store';
 
 import "./StepTwo.css"
+import PetsIcon from '@mui/icons-material/Pets';
+import DrawIcon from '@mui/icons-material/Draw';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import ForestIcon from '@mui/icons-material/Forest';
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import LanguageIcon from '@mui/icons-material/Language';
+import ChurchIcon from '@mui/icons-material/Church';
 
 const StepTwo = () => {
   const [category, updateCategory] = useGiveBuddyStore(
@@ -29,6 +37,20 @@ const StepTwo = () => {
   //   console.log(category)
   // }, [category])
 
+  const categoryIcon = (name: string) => {
+    switch(name) {
+      case "Animal Welfare":   return <PetsIcon/>;
+      case "Arts & Culture":   return <DrawIcon/>;
+      case "Education":   return <LocalLibraryIcon/>;
+      case "Environment":   return <ForestIcon/>;
+      case "Health":   return <MedicalInformationIcon/>;
+      case "Social Services":   return <PeopleAltIcon/>;
+      case "International Aid":   return <LanguageIcon/>;
+      case "Religion":   return <ChurchIcon/>;
+      default:  return null
+    }
+  }
+
   return (
     <div id="onboarding-step-two">
       {categories.map((category) => {
@@ -39,6 +61,8 @@ const StepTwo = () => {
             onClick={() => handleClick(category.name)}
             style={{color: selectCategory.includes(category.name) ? "white" : "#4E4E4E", backgroundColor: selectCategory.includes(category.name) ? "#254139": "white"}}
           >
+            {categoryIcon(category.name)}
+            &nbsp;
             {category.name}
           </Button>
         )
